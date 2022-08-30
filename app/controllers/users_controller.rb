6d583b53077b6b1251dc7d00eabc045c1f550c2b
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-30 16:18:40 -0500

    commit tree 0c88a7b5cc5f0474d7d2cdb8cfd01f86ea6eebc5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
+    rescue ActiveRecord::RecordNotFound => error
+      render json: { errors: error.message }, status: :not_found
   end
 
 end
